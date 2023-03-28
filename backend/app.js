36c19027e9be7aa599c0241a7d00d8c5a97dd2fe
abc <soo990414@edu.dju.ac.kr>
2023-03-28 15:25:39 +0900

    익스프레스

---
+++ backend/app.js
@@ -0,0 +1,55 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//vue연결
+app.use(express.static(path.join(__dirname, '..', 'frontEnd', 'web', 'dist')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Vue 애플리케이션의 라우팅 처리를 위한 핸들러 등록
+app.get('/vue', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'frontEnd', 'web', 'dist', 'index.html'));
+});
+
+//서버 시작
+app.listen(3000, () => {
+  console.log('Server is running on port 3000.');
+});
+
+module.exports = app;
+
